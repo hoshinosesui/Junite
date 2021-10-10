d343ddf54ed91e6596b14143aaa67947543c85e3
hoshinosesui <hoshinose2021@gmail.com>
2021-10-10 17:16:20 +0900

    Setting Movement

--- Junite/Assets/Application/Scripts/PlayerController.cs
+++ Junite/Assets/Application/Scripts/PlayerController.cs
@@ -5,7 +5,13 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Animator animator;
-    private 
+
+    //入力用変数
+    private float verticalInput;
+    private float horizontalInput;
+
+    public float moveSpeed;
+    public float rotateSpeed;
 
     // Start is called before the first frame update
     void Start()
@@ -16,9 +22,25 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey("w"))
+        //入力を受ける
+        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        //前後移動のアニメーション
+        animator.SetFloat("Speed", verticalInput);
+
+        //前後移動
+        //後ろに移動する際には移動速度が半分
+        if(verticalInput >= -0.5)
         {
-            animator.SetBool("isWalk", true);
+            transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed * verticalInput);
+        } 
+        else if (verticalInput < -0.5)
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed * verticalInput * 0.5f);
         }
+
+        //左右移動
+        transform.Rotate(Vector3.up * Time.deltaTime * rotateSpeed * horizontalInput);
     }
 }
